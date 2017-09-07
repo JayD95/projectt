35ab98404da7eeda8f4f3095432b4ef65aa57846
JayD95 <jay.dammers@student.hu.nl>
2017-09-07 12:50:41 +0200

    Salaris opdracht

---
+++ Opdracht/Les2_4.py
@@ -0,0 +1,4 @@
+verdien = eval(input('Wat verdien je per uur: '))
+gewerkt_uur = eval(input('Hoeveel uur heb je gewerkt: '))
+salaris = (verdien * gewerkt_uur)
+print ('Uw salaris is: ' + str(salaris) + ',-')
